dccd123e1eb9254998d9d705380a52b8d741c10b
SooHwan <newpoo@naver.com>
2022-01-17 18:38:07 +0900

    feat: 하단 탭 추가

--- app/src/main/java/com/example/water_drinking_whale/MainActivity.kt
+++ app/src/main/java/com/example/water_drinking_whale/MainActivity.kt
@@ -2,11 +2,51 @@ package com.example.water_drinking_whale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(supportFragmentManager.beginTransaction()) {
+            val homeFragment = HomeFragment()
+            replace(R.id.container, homeFragment)
+            commit()
+        }
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeTab -> {
+                    with(supportFragmentManager.beginTransaction()) {
+                        val homeFragment = HomeFragment()
+                        replace(R.id.container, homeFragment)
+                        commit()
+                    }
+
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.logTab -> {
+                    with(supportFragmentManager.beginTransaction()) {
+                        val logFragment = LogFragment()
+                        replace(R.id.container, logFragment)
+                        commit()
+                    }
+
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.noticeTab-> {
+                    with(supportFragmentManager.beginTransaction()) {
+                        val noticeFragment = NoticeFragment()
+                        replace(R.id.container, noticeFragment)
+                        commit()
+                    }
+
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
     }
 }

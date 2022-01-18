4fb61c2938bac79f57c33880c1e0cf89cdfae962
sooziini <soojinpar1026@gmail.com>
2022-01-18 16:22:44 +0900

    fix: gradle 버전 수정

--- app/src/main/java/com/example/water_drinking_whale/MainActivity.kt
+++ app/src/main/java/com/example/water_drinking_whale/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.water_drinking_whale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

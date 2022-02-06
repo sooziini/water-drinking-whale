e226002a57813a0c53c00d051013b6135a1d09c8
sooziini <soojinpar1026@gmail.com>
2022-02-06 16:58:15 +0900

    feat: home fragment toolbar 추가

--- app/src/main/java/com/example/water_drinking_whale/HomeFragment.kt
+++ app/src/main/java/com/example/water_drinking_whale/HomeFragment.kt
@@ -1,10 +1,9 @@
 package com.example.water_drinking_whale
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.water_drinking_whale.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,7 +21,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // 코드 작성
+        setToolbar()
+
+    }
+
+    private fun setToolbar() {
+        binding.homeFragmentToolbar.apply {
+            inflateMenu(R.menu.home_fragment_menu)
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.homeSetting -> {
+                        startActivity(Intent(activity, SettingActivity::class.java))
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {

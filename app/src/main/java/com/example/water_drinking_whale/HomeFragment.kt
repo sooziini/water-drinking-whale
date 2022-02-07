7400051f8019f1551f2942e844ac595a34c38f48
sooziini <soojinpar1026@gmail.com>
2022-02-08 01:38:55 +0900

    feat: 물마시기 dialog 추가

--- app/src/main/java/com/example/water_drinking_whale/HomeFragment.kt
+++ app/src/main/java/com/example/water_drinking_whale/HomeFragment.kt
@@ -3,7 +3,10 @@ package com.example.water_drinking_whale
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.example.water_drinking_whale.databinding.DialogHomeBinding
 import com.example.water_drinking_whale.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,6 +26,9 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setToolbar()
 
+        binding.homeFragmentBtn.setOnClickListener {
+            setWaterIntakeDialog()
+        }
     }
 
     private fun setToolbar() {
@@ -35,6 +41,33 @@ class HomeFragment : Fragment() {
                         true
                     }
                     else -> false
+                }
+            }
+        }
+    }
+
+    private fun setWaterIntakeDialog() {
+        val dialogBinding = DialogHomeBinding.inflate(layoutInflater)
+        val intakeEt = dialogBinding.dialogEt
+
+        val builder = context?.let { AlertDialog.Builder(it) }
+        builder?.apply {
+            setMessage(R.string.home_today_intake)
+            setView(dialogBinding.root)
+            setCancelable(false)
+            setPositiveButton(R.string.positive_btn, null)
+            setNegativeButton(R.string.negative_btn, null)
+        }!!.create().apply {
+            show()
+            getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                val todayIntake = binding.homeFragmentTodayIntakeTv
+                if (intakeEt.text.toString() == "") {
+                    Toast.makeText(context, "물 섭취량을 입력해 주세요.", Toast.LENGTH_SHORT).show()
+                } else {
+                    val total = todayIntake.text.toString().toInt() + intakeEt.text.toString().toInt()
+                    todayIntake.text = total.toString()
+                    // TODO: 오늘의 물 섭취량(total) db update 해야 함
+                    dismiss()
                 }
             }
         }
